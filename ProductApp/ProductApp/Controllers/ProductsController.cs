a5bebe7c380990d0801e2410e6f09384d0ffb9b8
Ajinkya Bhagwat <ajinkya.bhagwat@gmail.com>
2016-04-09 13:30:39 +0530

    added pizza

--- ProductApp/ProductApp/Controllers/ProductsController.cs
+++ ProductApp/ProductApp/Controllers/ProductsController.cs
@@ -10,11 +10,12 @@ namespace ProductApp.Controllers
 {
     public class ProductsController : ApiController
     {
-        Product[] products = new Product[]
+        private Product[] products = new Product[]
         {
-            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
-            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
-            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+            new Product {Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1},
+            new Product {Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M},
+            new Product {Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M},
+            new Product {Id = 4, Name = "Pizza", Category = "Food", Price = 1.5M}
         };
 
         public IEnumerable<Product> GetAllProducts()
